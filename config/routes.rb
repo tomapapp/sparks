a0a3bbae47c4ad539b572d33e4083cf123ea4e95
Marco de Biolley <marcdebiolley@gmail.com>
2020-03-24 18:36:22 +0100

    first step of the sign up nearly done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :users
+
+  get '/edit_preferences', to: "pages#my_preferences"
+  get '/edit_dateinfo', to: "pages#my_dateinfo"
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
