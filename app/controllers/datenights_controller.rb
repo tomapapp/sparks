132f89f91e2b5a1553b78ce1970999a5684c9bc1
Tamas Papp <toma.papp92@gmail.com>
2020-03-24 16:39:32 +0100

    Datenights controller created

---
+++ app/controllers/datenights_controller.rb
@@ -0,0 +1,2 @@
+class DatenightsController < ApplicationController
+end
---
+++ test/controllers/datenights_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DatenightsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
