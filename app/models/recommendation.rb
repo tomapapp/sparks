010e7551d7ed12df069fb0fdd1e5e7ce6cf0b28b
Tamas Papp <toma.papp92@gmail.com>
2020-03-25 16:03:01 +0100

    CLoudinary setup and image upload enabled

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -3,4 +3,5 @@ class Recommendation < ApplicationRecord
   has_many :times #dependent: :destroy
   has_many :dates #dependent: :destroy
   has_many :datenights, dependent: :destroy
+  has_one_attached :photo
 end
