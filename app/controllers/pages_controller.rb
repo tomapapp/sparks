13065e4d878e53bbd82b61d95fada45cf3ee93db
Raphael Daverio <raphael.daverio@gmail.com>
2020-03-25 17:58:15 +0100

    added logic to save preferences

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,16 @@ class PagesController < ApplicationController
   end
 
   def my_preferences
+    # getting all the categories to display them in the form
     @categories = Category.all
+  end
+
+  def add_preferences
+    params[:user][:preferences].each do |preference|
+      category = Category.find_by(name: preference)
+      Preference.create(user: current_user, category: category)
+    end
+    redirect_to edit_date_info_path
   end
 
   def my_date_info
