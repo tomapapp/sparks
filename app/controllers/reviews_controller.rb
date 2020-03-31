45f58ba9b280c39f36b914ee647a4db6823ca988
Raphael Daverio <raphael.daverio@gmail.com>
2020-03-31 16:39:36 +0200

    added the reviews page with basic styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,23 @@
 class ReviewsController < ApplicationController
+  before_action :set_datenight, only: [:new, :create]
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
   end
+
+  private
+
+  def set_datenight
+    @datenight = Datenight.find(params[:datenight_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :date_rating)
+  end
+
+
 end
